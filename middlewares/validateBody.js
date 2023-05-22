724fc76bb43b46f73a823ed936be62068e66ade8
Diego <diegoguatedb2002@gmail.com>
2023-05-22 15:27:54 -0600

    Eliminar archivo si no cumple con shema del body

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -1,8 +1,28 @@
+import fs from 'fs';
+
 export default (schema) => async (req, res, next) => {
   try {
     await schema.validate(req.body);
     return next();
   } catch (err) {
+    // Eliminar archivos cargados
+    const { uploadedFiles } = req;
+    if (uploadedFiles !== null && uploadedFiles !== undefined) {
+      if (Array.isArray(uploadedFiles)) {
+        uploadedFiles.forEach((file) => {
+          fs.unlink(`${global.dirname}/files/${file}`, (error) => {
+            // eslint-disable-next-line no-console
+            console.log('🚀 ~ file: validateBody.js:15 ~ fs.unlink ~ error:', error);
+          });
+          return null;
+        });
+      } else {
+        fs.unlink(`${global.dirname}/files/${uploadedFiles}`, (error) => {
+        // eslint-disable-next-line no-console
+          console.log('🚀 ~ file: validateBody.js:21 ~ error:', error);
+        });
+      }
+    }
     res.statusMessage = err.message;
     return res.status(400).send({ err: err.message, status: 400, ok: false });
   }
