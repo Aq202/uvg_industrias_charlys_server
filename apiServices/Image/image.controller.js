2b19b9c4ead6f6b98e353ae100174409513527de
Diego <diegoguatedb2002@gmail.com>
2023-05-23 13:07:46 -0600

    Obtener imagenes de solicitud de pedidos de buckets

--- apiServices/Image/image.controller.js
+++ apiServices/Image/image.controller.js
@@ -1,7 +1,19 @@
-const uploadImageController = (req, res) => {
-  console.log(req.uploadedFiles);
-  res.sendStatus(200);
+import getFileFromBucket from '../../services/cloudStorage/getFileFromBucket.js';
+import consts from '../../utils/consts.js';
+
+/* eslint-disable no-restricted-syntax */
+const getOrderRequestImageController = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const fileResult = await getFileFromBucket(`${consts.bucketRoutes.orderRequest}/${id}`);
+    res.write(fileResult, 'binary');
+    res.end(null, 'binary');
+  } catch (ex) {
+    console.log(ex);
+    res.sendStatus(404);
+  }
 };
 
 // eslint-disable-next-line import/prefer-default-export
-export { uploadImageController };
+export { getOrderRequestImageController };
--- apiServices/Image/image.route.js
+++ apiServices/Image/image.route.js
@@ -1,12 +1,9 @@
 import express from 'express';
-import { uploadImageController } from './image.controller.js';
-import uploadClientImage from '../../services/uploadFiles/uploadImage.js';
-import multerMiddleware from '../../middlewares/multerMiddleware.js';
-import validateBody from '../../middlewares/validateBody.js';
-import loginSchema from '../../utils/validationSchemas/loginSchema.js';
+import { getOrderRequestImageController } from './image.controller.js';
+import ensureAdminAuth from '../../middlewares/ensureAdminAuth.js';
 
 const imageRouter = express.Router();
 
-imageRouter.post('/', multerMiddleware(uploadClientImage.any()), validateBody(loginSchema), uploadImageController);
+imageRouter.get('/orderRequest/:id', ensureAdminAuth, getOrderRequestImageController);
 
 export default imageRouter;
