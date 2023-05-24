61a457d09e9e60077470aae02cbea82da9c2b27f
pabloozamora <pabloozamora02@gmail.com>
2023-05-24 05:29:02 -0600

    Actualizar inventario

--- apiServices/inventory/inventory.route.js
+++ apiServices/inventory/inventory.route.js
@@ -1,12 +1,19 @@
 import express from 'express';
 import validateBody from '../../middlewares/validateBody.js';
 import newInventoryElementSchema from '../../utils/validationSchemas/newInventoryElementSchema.js';
-import { getInventoryController, newInventoryElementController } from './inventory.controller.js';
+import {
+  getInventoryController,
+  newInventoryElementController,
+  getInventorybyIdController,
+  updateInventoryElementController,
+} from './inventory.controller.js';
 import ensureAdminAuth from '../../middlewares/ensureAdminAuth.js';
 
 const inventoryRouter = express.Router();
 
 inventoryRouter.post('/', validateBody(newInventoryElementSchema), newInventoryElementController);
 inventoryRouter.get('/', ensureAdminAuth, getInventoryController);
+inventoryRouter.get('/id', ensureAdminAuth, getInventorybyIdController);
+inventoryRouter.put('/update', ensureAdminAuth, updateInventoryElementController);
 
 export default inventoryRouter;
