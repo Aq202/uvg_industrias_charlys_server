d6be97e270f65c36296ac8ade973657b496eafca
Diego <diegoguatedb2002@gmail.com>
2023-07-16 18:24:03 -0600

    Reorganizacion de archivos sql

--- database/dbModel.sql
+++ database/dbModel.sql
@@ -1,4 +1,6 @@
-drop table if exists user_account, client_organization, employee, session, order_request, "order" CASCADE;
+drop table if exists order_request, order_request_media user_account, client_organization, employee,
+"session", order_request, "order", order_detail, inventory, requirements, product, product_type, 
+"size", material, fabric CASCADE;
 
 CREATE TABLE user_account(
 	id_user VARCHAR(15) PRIMARY KEY,
@@ -39,3 +41,73 @@ deadline date,
 details text
 );
 
+CREATE TABLE "size"(
+	"id_size" VARCHAR(15) PRIMARY KEY,
+	"size" VARCHAR(10)
+);
+
+CREATE TABLE product_type(
+	id_product_type VARCHAR(15) PRIMARY KEY,
+	"name" VARCHAR(100)
+);
+
+CREATE TABLE product(
+	id_product VARCHAR(15) PRIMARY KEY,
+	"type" VARCHAR(15),
+	client VARCHAR(15),
+	color VARCHAR(100)
+);
+
+CREATE TABLE order_detail(
+	no_order VARCHAR(15),
+	product VARCHAR(15),
+	"size" VARCHAR(15),
+	quantity INT,
+	PRIMARY KEY(no_order, product, "size")
+);
+
+	CREATE TABLE material(
+		id_material VARCHAR(15) PRIMARY KEY,
+		name VARCHAR(200) NOT NULL,
+		supplier VARCHAR(100),
+		color VARCHAR(100),
+		type INTEGER NOT NULL
+	);
+
+	CREATE TABLE material_type(
+		id_material_type SERIAL PRIMARY KEY,
+		name VARCHAR(200) NOT NULL
+	);
+
+
+CREATE TABLE inventory(
+	id_inventory VARCHAR(15) PRIMARY KEY,
+	material VARCHAR(15) UNIQUE,
+	product VARCHAR(15) UNIQUE,
+	quantity FLOAT NOT NULL DEFAULT 0,
+	measurement_unit VARCHAR(100) NOT NULL,
+	details VARCHAR(500)
+);
+
+CREATE TABLE requirements(
+	product VARCHAR(15),
+	"size" VARCHAR(15),
+	material VARCHAR(15),
+	fabric VARCHAR(15),
+	quantity_per_unit FLOAT
+);
+
+create table order_request(
+no_request varchar(15) primary key,
+customer_name VARCHAR(100) NOT NULL,
+customer_email VARCHAR(100),
+customer_phone VARCHAR(100),
+customer_address VARCHAR(300) NOT null,
+description text,
+date_placed date
+);
+
+CREATE TABLE order_request_media(
+  no_request varchar(15) NOT NULL,
+  name varchar(1000) NOT NULL
+);
