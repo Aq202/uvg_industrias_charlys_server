aacc1be067be735e6bb5ce33a7fa84aca90b27be
pabloozamora <pabloozamora02@gmail.com>
2023-07-21 14:17:30 -0600

    Correcciones delOrganization

--- apiServices/organization/organization.model.js
+++ apiServices/organization/organization.model.js
@@ -139,14 +139,12 @@ const updateOrganization = async ({
 
 const deleteOrganization = async ({ id }) => {
   try {
-    const sql = 'DELETE FROM client_organization WHERE id_client_organization = $1';
+    const sql = 'UPDATE client_organization SET enabled = false WHERE id_client_organization = $1';
     const { rowCount } = await query(sql, id);
     if (rowCount !== 1) throw new CustomError('No se encontró la organización.', 400);
   } catch (ex) {
-    if (ex?.code === '23503') {
-      const sqlDisable = 'UPDATE client_organization SET enabled = false WHERE id_client_organization = $1';
-      query(sqlDisable, id);
-    } else throw ex;
+    if (ex instanceof CustomError) throw ex;
+    throw ex;
   }
 };
 
@@ -155,7 +153,7 @@ const getOrganizations = async ({ page }) => {
   const sql1 = 'SELECT COUNT(*) FROM client_organization';
   const { result: resultCount, rowCount: rowCount1 } = await query(sql1);
   if (rowCount1 === 0) throw new CustomError('No se encontraron resultados.', 404);
-  const sql2 = `SELECT * FROM client_organization ORDER BY id_client_organization LIMIT ${consts.pageLength} OFFSET ${offset}`;
+  const sql2 = `SELECT * FROM client_organization WHERE enabled = true ORDER BY id_client_organization LIMIT ${consts.pageLength} OFFSET ${offset}`;
   const { result, rowCount: rowCount2 } = await query(sql2);
   if (rowCount2 === 0) throw new CustomError('No se encontraron resultados.', 404);
   const response = result.map((val) => ({
