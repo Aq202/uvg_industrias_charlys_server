580d7d1ff520c046ceb5f87599502a4bcb5bebde
Erick Guerra <erickguerra2201@gmail.com>
2023-09-21 22:03:48 -0600

    Eliminación de console.log

--- apiServices/order/order.controller.js
+++ apiServices/order/order.controller.js
@@ -59,7 +59,6 @@ const getOrderByIdController = async (req, res) => {
     const result = await getOrderById(orderId);
     res.send(result);
   } catch (ex) {
-    console.log(ex);
     let err = 'Ocurrio un error al obtener la información de este pedido.';
     let status = 500;
     if (ex instanceof CustomError) {
